32b927fcd81bfd0df3e258f8efd3509275a94a5b
3BoOoD97 <abodn70@hotmail.com>
2021-07-19 15:45:34 +0200

    Handling invalid input in create page

--- routes/even-routes.js
+++ routes/even-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { event, get } = require("jquery");
 const router = express.Router();
 const Event = require('../models/event')
+const { check, validationResult } = require('express-validator');
 
 //route to home event
 router.get('/', (req,res)=> {
@@ -22,29 +23,47 @@ router.get('/', (req,res)=> {
 
 // create a new event
 router.get('/create', (req,res)=> {
-res.render('event/create')
+res.render('event/create',{
+    errors:false
+
+})
 })
 
 //save event to DB
-router.post('/create', (req,res)=> {
-    console.log(req.body)
+router.post('/create', [
+    check('title').isLength({min: 5}).withMessage('Title should be more than five'),
+    check('description').isLength({min: 5}).withMessage('description should be more than five'),
+    check('location').isLength({min: 5}).withMessage('location should be more than five'),
+    check('date').isLength({min: 5}).withMessage('Date should be valid'),
 
-    let newEvent = new Event({
-        title: req.body.title,
-        description: req.body.description,
-        location: req.body.location,
-        date: req.body.date,
-        created_at: Date.now()
-    })
-    newEvent.save((err)=>{
-        if(!err){
-            console.log("ADD")
-            res.redirect('/events')
-        }
-        else{
-            console.log("error")
-        }
-    })
+] ,(req,res)=> {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()){
+
+        res.render('event/create', {
+            errors: errors.array()
+        })
+    }
+    
+    else {
+        let newEvent = new Event({
+            title: req.body.title,
+            description: req.body.description,
+            location: req.body.location,
+            date: req.body.date,
+            created_at: Date.now()
+        })
+    
+        newEvent.save((err)=>{
+            if(!err){
+                console.log("ADDED")
+                res.redirect('/events')
+            }
+            else{
+                console.log("error")
+            }
+        })    }
   })
 
 //show single event
